367a31e817c8702d8dab69a8e4929a5d8264c2a2
nealgarcia <nathnlgarcia@gmail.com>
2022-02-18 11:24:28 -0600

    update README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# YEEZY Notepad
+Create your own Kanye notepad meme with Yeezy notepad. Type out and submit any text to see it on Kanye's notepad. Click 'Save Image' to save the picture to your computer. 
+
+If you haven't seen the memes, check out these links:
+
+- [Kanye West holding a scrawled-on notepad has become Twitter's new favorite meme](https://ftw.usatoday.com/lists/kanye-west-notepad-memes-instagram-twitter)
+- [Kanye West Shared an Image of Himself With a Notepad, and Memes Were Born](https://www.distractify.com/p/kanye-west-notepad-meme)
+- [Kanye West Holding Notepad](https://knowyourmeme.com/memes/kanye-west-holding-notepad)
+
+Made with HTML, Sass, and JavaScript and html2canvas
+
+## Font
+https://github.com/mcansh/yeezy-logo-font
--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+
 const imgContainer = document.querySelector("#imgContainer")
 const form = document.querySelector("#form")
+const saveButton = document.querySelector("#saveButton")
 let inputField = document.querySelector("#input")
 
+// Get data from form and append on image
 form.addEventListener('submit', (event) => {
+    // If element already exists, call removePrevious Function
     removePrevious()
     event.preventDefault()
     let input = inputField.value
 
+    // Create new <p> element and append into image container
     let text = document.createElement("p")
     text.innerText = input
     text.setAttribute("id", "notepadText")
@@ -14,10 +19,18 @@ form.addEventListener('submit', (event) => {
 })
 
 
-// Remove previous element
+// Remove previous element from DOM
 function removePrevious(){
     // if element already exists in document, remove it
     var elem = document.querySelector("#notepadText")
     if(document.body.contains(elem))
         elem.parentNode.removeChild(elem)
 }
+
+saveButton.addEventListener('click', (event) => {
+    console.log("test")
+    html2canvas(imgContainer).then(function(canvas){
+        imgContainer.appendChild(canvas)
+    })
+})
+
