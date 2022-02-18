37113c90492430bab7e099a78ae15c5ac2c4e6fe
nealgarcia <nathnlgarcia@gmail.com>
2022-02-17 21:43:18 -0600

    input data is captured on submit

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const form = document.querySelector(".form")
-form.addEventListener()
+const imgContainer = document.querySelector("#imgContainer")
+const form = document.querySelector("#form")
+let input = document.querySelector("#input")
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let text = input.value
+    console.log(text)
+})
+
+var text = document.createElement("p")
+text.innerHTML = input.value;
